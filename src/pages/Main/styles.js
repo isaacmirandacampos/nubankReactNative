4bd415484a9868ef1513266bb8fac993c82c23ec
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-10-09 18:23:12 -0300

    atualizando background

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -2,16 +2,16 @@ import { StyleSheet } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#8B10AE",
+    paddingTop: getStatusBarHeight(),
+    justifyContent: "center",
+  },
   content: {
     flex: 1,
     maxHeight: 400,
     zIndex: 5,
   },
-  Container: {
-    flex: 1,
-    backgroundColor: "#8B10AE",
-    paddingTop: getStatusBarHeight(),
-    justifyContent: "center",
-  }
 
 });
