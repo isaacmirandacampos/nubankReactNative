3ba5f1d3b79d78f2b5e8fbc7056538fcb4966147
isaacmirandacampos <isaacdmcampos@outlook.com>
2019-10-09 18:32:46 -0300

    arrumando repositório

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import QRCode from 'react-native-qrcode';
 
 import { View } from 'react-native';
+
 
 export default function Menu() {
 
   return (
-    <View />
+    <View>
+      <View>
+        <QRCode
+          value="https://github.com/isaacmirandacampos"
+          size={80}
+          bgColor="#FFF"
+          fgColor="#8B10AE"
+        />
+      </View>
+    </View>
 
   );
 };
